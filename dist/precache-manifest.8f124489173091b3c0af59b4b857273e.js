1a71843a2b4ba0884f52e5510f14670b8dde286d
Vigneshwaran S <58353559+vigneshwaran-dev@users.noreply.github.com>
2021-03-15 12:14:37 +0530

    v . 2.1.0

---
+++ dist/precache-manifest.8f124489173091b3c0af59b4b857273e.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6689638f3570144bae7",
+    "url": "/css/app.94568693.css"
+  },
+  {
+    "revision": "d91f455390d02ea92dbbd6c6b89e66b8",
+    "url": "/img/boy1.d91f4553.jpg"
+  },
+  {
+    "revision": "8102985b5fbb8fd93163f6b4981cb02a",
+    "url": "/img/girl.8102985b.jpg"
+  },
+  {
+    "revision": "f71669d68bc0df77a4ac443a687f7416",
+    "url": "/img/rope.f71669d6.png"
+  },
+  {
+    "revision": "c0a59e5bbcd0762322a8476601522618",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f6689638f3570144bae7",
+    "url": "/js/app.c4701dd9.js"
+  },
+  {
+    "revision": "0d30d5fa5f76690633c8",
+    "url": "/js/chunk-vendors.919c39de.js"
+  },
+  {
+    "revision": "2d3019ac534eceda1b72c288777870a3",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "a7a990d5e3a76f74c0a691f1df4aebd2",
+    "url": "/media/bell.a7a990d5.mp3"
+  },
+  {
+    "revision": "bf9c19c3bcc2180a9538083a22b0e643",
+    "url": "/media/sai_gayathri.bf9c19c3.mp3"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
